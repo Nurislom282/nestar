ff68b346bdeeacce92686694f14870fe821a374c
Nurislom282 <anurislom425@gmail.com>
2025-09-11 00:43:18 +0500

    fix: initiallation packages & modify format standards

--- apps/nestars-batch/src/main.ts
+++ apps/nestars-batch/src/main.ts
@@ -3,6 +3,6 @@ import { NestarsBatchModule } from './nestars-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NestarsBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.SERVER_BATCH ?? 3000);
 }
 bootstrap();
--- apps/nestars-batch/src/nestars-batch.module.ts
+++ apps/nestars-batch/src/nestars-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestarsBatchController } from './nestars-batch.controller';
 import { NestarsBatchService } from './nestars-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestarsBatchController],
   providers: [NestarsBatchService],
 })
