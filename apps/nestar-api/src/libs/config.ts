321f4c05c08ac7f715261a695f66da84f3dc3cf5
Nurislom <anurislom425@gmail.com>
2025-09-25 14:44:48 +0500

    feat: develop getAllMembersByAdmin and updateMemberByAdmin APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -2,6 +2,8 @@ import { ObjectId } from 'bson';
 
 export const availableAgentSorts = ["createdAt","updatedAt","memberLikes","memberView","memberRank"]
 
+export const availableMemberSorts = ["createdAt", "updatedAt", "memberLikes", "memberView",]
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
